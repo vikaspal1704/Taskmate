0f26719ef2dd9faa45f400211eecd3fb6c08bb04
Vikas <palv499@gmail.com>
2020-08-13 14:37:33 +0530

    Updated Files

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -1,4 +1,4 @@
-
+import django_heroku
 import os
 import environ
 
@@ -121,3 +121,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())

d03a131ff5e8d6da022faefb4d3264d9ab8b0d82
Vikas Pal <palv499@gmail.com>
2020-08-17 10:08:41 +0530

    Updated settings.py removed spaces

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -13,12 +13,12 @@ environ.Env.read_env(os.path.join(BASE_DIR,'.env'))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY=env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DJANGO_DEBUG')
+DEBUG=env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = [env('DJANGO_ALLOWED_HOSTS')]
+ALLOWED_HOSTS=[env('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
